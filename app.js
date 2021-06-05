3d22096484707d5b6a31f1b5c981433c86d7f564
rubtal82 <rubtal82@gmail>
2021-06-04 22:07:25 -0400

    version 1

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const hbs = require('hbs');
+const path = require('path');
+const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+const app = express();
+const punkAPI = new PunkAPIWrapper();
+
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/beers', (req, res,) => {
+  
+     punkAPI.getBeers()
+    .then(beersFromApi => { 
+    res.render('beers', {beers:beersFromApi}) 
+    })
+    .catch(error => console.log(error));
+   
+});
+
+app.get('/randombeer', (req, res) => {
+
+     punkAPI.getRandom()
+     .then(randomFromApi => {
+     res.render('randombeer', {randombeer:randomFromApi[0]})
+    })
+    
+    .catch(error => console.log(error));
+});
+app.listen(3000, () => console.log('🏃‍ on port 3000'));
